c002160979dd184a3a854c4f39952913371bb61c
Ivo Nascimento <ivo.nascimento@e-smart.com.br>
2014-03-27 11:31:12 -0300

    a simple container for config

---
+++ tests/Payment/ConfigTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Ecomz\Payment;
+
+class ConfigTest extends \PHPUnit_Framework_TestCase{
+
+	public function testSetGet()
+	{
+		$config = new Config();
+		$expected =  array('name'=>'propertyOne', 'value'=>'valueOne');
+		$config->set($expected['name'],$expected['value']);
+		$this->assertEquals($expected['value'], $config->get($expected['name']));
+	}
+	public function testGetNotExistentName()
+	{
+		$config = new Config();
+		$this->assertTrue( is_null($config->get('NameNotSet')));
+	}
+	public function testLoadFromFileTest()
+	{
+		$expected = array('nome'=> 'ivo', 'sobrenome'=> 'nascimento');
+		$file = __DIR__."/../resources/confitests.php";
+		$config = Config::fromFile($file, 'TEST');
+		$this->AssertEquals($expected['nome'], $config->get('nome'));
+		$this->AssertEquals($expected['sobrenome'], $config->get('sobrenome'));
+	}
+	public function testLoadFromFileOther()
+	{
+		$expected = array('nome'=> 'andre', 'sobrenome'=> 'nascimento');
+		$file = __DIR__."/../resources/confitests.php";
+		$config = Config::fromFile($file, 'OTHER');
+		$this->AssertEquals($expected['nome'], $config->get('nome'));
+		$this->AssertEquals($expected['sobrenome'], $config->get('sobrenome'));
+	}
+
+	/**
+	* @expectedException Ecomz\Payment\Exception
+	* @expectedExceptionMessage Config File Not Found
+	*/	
+	public function testLoadFromFileNotValidFile()
+	{
+		$file = __DIR__."/../resources/invalidconfig.php";
+		$config = Config::fromFile($file, 'OTHER');
+	}
+
+	/**
+	* @expectedException Ecomz\Payment\Exception
+	* @expectedExceptionMessage Config $env Not Found
+	*/	
+	public function testLoadFromFileNotValidEnvironment()
+	{
+		$file = __DIR__."/../resources/confitests.php";
+		$config = Config::fromFile($file, 'OTHER2');
+	}
+
+	public function testFromArray()
+	{
+		$expected = array('nome'=> 'andre', 'sobrenome'=> 'nascimento');
+		$config  = new Config();
+		$config->fromArray($expected);
+		$this->AssertEquals($expected['nome'], $config->get('nome'));
+		$this->AssertEquals($expected['sobrenome'], $config->get('sobrenome'));
+	}
+}
